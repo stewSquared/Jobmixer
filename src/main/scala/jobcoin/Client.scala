b0118a7326d8f617ebf3809dff4f3f328c4a5f12
Stewart Stewart <stewinsalot@gmail.com>
2018-04-19 21:51:12 -0700

    Implement a Client fake

--- src/main/scala/jobcoin/Client.scala
+++ src/main/scala/jobcoin/Client.scala
@@ -1,6 +1,8 @@
 package jobcoin
 
-import java.util.concurrent.Future
+import java.time.Instant
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 case class AddressInfo(
   balance: Jobcoin,
@@ -13,4 +15,45 @@ trait Client {
 
   // TODO: Can the transaction timestamp be retrieved via callback? If not, use `Unit`
   def send(from: Address, to: Address, amount: Jobcoin): Future[Transaction]
+}
+
+class FakeClient extends Client {
+  import collection.mutable.Map
+
+  // Note: Unsynchronized mutations here
+  private var ledger: List[Transaction] = Nil
+
+  // Note: Unsynchronized mutation here
+  private val balances = new Map.WithDefault(Map.empty[Address, Jobcoin], (_ : Address) => BigDecimal(0))
+
+  override def addressInfo(address: Address) = Future.successful(
+    AddressInfo(
+      balances(address),
+      ledger.filter(txn => txn.to == address || txn.from == Some(address))))
+
+  override def transactions = Future.successful(ledger)
+
+  override def send(from: Address, to: Address, amount: Jobcoin) = {
+    require(amount > 0, "Amount must be over 0")
+    require(balances(from) >= 0, s"$from has no jobcoins!  Is it a new or unused address?")
+    require(balances(from) >= amount, s"$from only has ${balances(from)} jobcoins!")
+
+    Future[Transaction] {
+      balances(from) -= amount
+      balances(to) += amount
+      val newTransaction = Transaction(Instant.now(), Some(from), to, amount)
+      ledger = newTransaction :: ledger
+
+      newTransaction
+    }
+  }
+
+  // TODO: move to trait and return Future
+  def create(address: Address): Unit = {
+    val amount: Jobcoin = BigDecimal(50)
+    balances(address) += 50
+    // TODO add a constructor with timestamp truncated to millis
+    val initialDeposit = Transaction(Instant.now(), from = None, to = address, amount)
+    ledger = initialDeposit :: ledger
+  }
 }
