ea027f8859beb75e5f5fd464d9d390e65fe0bfd5
Stewart Stewart <stewinsalot@gmail.com>
2018-05-09 18:43:44 -0700

    Fix ClientSpec to account for initial transaction when creating JBC

--- src/test/scala/jobcoin/ClientSpec.scala
+++ src/test/scala/jobcoin/ClientSpec.scala
@@ -1,21 +1,31 @@
 package jobcoin
 
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{ FlatSpec, Matchers }
+import org.scalatest.{ Inside, FlatSpec, Matchers }
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class ClientSpec extends FlatSpec with Matchers with ScalaFutures {
+class ClientSpec extends FlatSpec with Matchers with ScalaFutures with Inside {
   val client = new FakeClient()
 
   "New address" should "have 0 balance and no transactions" in {
     client.addressInfo(newAddress()).futureValue shouldBe AddressInfo(0, Nil)
   }
 
-  it should "be able to start with new coins" in {
+  it should "be able to be loaded with new coins" in {
     val addr = newAddress()
     client.create(addr)
 
-    client.addressInfo(addr).futureValue shouldBe AddressInfo(50, Nil)
+    whenReady(client.addressInfo(addr)) {
+      x => inside(x) { case AddressInfo(balance, List(initialTransaction)) =>
+        balance shouldBe BigDecimal(50)
+        inside(initialTransaction) {
+          case Transaction(_, from, to, amount) =>
+            amount shouldBe balance
+            from shouldBe None
+            amount shouldBe balance
+        }
+      }
+    }
   }
 
   it should "be able to recive new coins" in {
@@ -24,12 +34,16 @@ class ClientSpec extends FlatSpec with Matchers with ScalaFutures {
     client.create(addr1)
 
     (for {
-      txn <- client.send(from = addr1, to = addr2, BigDecimal(20))
+      sendTxn <- client.send(from = addr1, to = addr2, BigDecimal(20))
       info1 <- client.addressInfo(addr1)
       info2 <- client.addressInfo(addr2)
     } yield {
-      info1 shouldBe AddressInfo(BigDecimal(30), transactions = List(txn))
-      info2 shouldBe AddressInfo(BigDecimal(20), transactions = List(txn))
+      inside(info1) {
+        case AddressInfo(balance, List(latest, create)) =>
+          balance shouldBe BigDecimal(30)
+          sendTxn shouldBe latest
+      }
+      info2 shouldBe AddressInfo(BigDecimal(20), transactions = List(sendTxn))
     }).futureValue
   }
 }
